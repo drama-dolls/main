9f2dbe239f164c3090f50f8a2a96e60c6b2e740b
non259315-commits <non259315@gmail.com>
2026-05-20 18:13:09 +0900

    feat:初期コード

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+
+import models
+from database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 # Reactからのアクセスを許可（CORS設定）
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"
+    allow_origins=["http://localhost:5173",
     "http://localhost:5174",], # Viteのデフォルトポート
     allow_methods=["*"],
     allow_headers=["*"],
@@ -15,3 +21,26 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI"}
+
+
+
+# ユーザーを1人登録する (POST)
+@app.post("/users/")
+def create_user(username: str, db: Session = Depends(get_db)):
+    # 新しいユーザーオブジェクトを作る
+    new_user = models.User(username=username, points=0)
+    # DBに追加してコミット（保存）
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user) # IDなどが確定した最新状態を反映
+    return new_user
+
+
+# ユーザー情報をIDで取得する (GET)
+@app.get("/users/{user_id}")
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    # DBから指定されたIDのユーザーを検索
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
---
+++ backend/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), nullable=False)
+    points = Column(Integer, nullable=False, default=0)
+    created_at = Column(DateTime, server_default=func.now())
